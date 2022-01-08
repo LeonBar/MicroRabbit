9268303f8ba2ed701466f649c1c313ac2ade1270
Leon Barkan <leon.barkan@gmail.com>
2022-01-08 11:21:03 +0200

    Implemented interfaces and classes for domain.core

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Commands/Command.cs
@@ -0,0 +1,15 @@
+﻿using MicroRabbit.Domain.Core.Events;
+using System;
+
+namespace MicroRabbit.Domain.Core.Commands
+{
+    public abstract class Command : Message
+    {
+        public DateTime Timestamp { get; protected set; } // protected set is for only who inherit from that class can set the value (abstract class!)
+
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+    }
+}
